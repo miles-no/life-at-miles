8de49f5f4a13e7fdc6eeb04aae197ce71f7263f8
Henry Sjøen <henry.sjoen@miles.no>
2025-04-11 22:17:23 +0200

    feat: add 404 page and remove unused blob background

--- app/routes.ts
+++ app/routes.ts
@@ -5,5 +5,7 @@ export default [
     route("login", "routes/login.tsx"),
     route("logout", "routes/logout.tsx"),
     route("auth/google", "routes/auth.google.tsx"),
-    route("auth/google/callback", "routes/auth.google.callback.tsx")
+    route("auth/google/callback", "routes/auth.google.callback.tsx"),
+    // Catch-all route for 404s
+    route("*", "routes/404.tsx")
 ] satisfies RouteConfig;
